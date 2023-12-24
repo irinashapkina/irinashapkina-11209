c16551b690815ebf9ee1737a47b9f567797d1b26
Irina Shapkina <irinkas2005@gmail.com>
2023-12-24 17:15:54 +0300

    create MyORM

---
+++ Homework_7/MyORM.Prog/MyORM.Prog/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyORM
+;
+[Table("Clients")]
+public class Clients
+{
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(10)]
+    [Column("gender", TypeName = "character varying(10)")]
+    public string Gender { get; set; }
+
+    [StringLength(255)]
+    [Column("full_name", TypeName = "character varying(255)")]
+    public string FullName { get; set; }
+
+    [Column("age")]
+    public int Age { get; set; }
+
+    [StringLength(255)]
+    [Column("contact_info", TypeName = "character varying(255)")]
+    public string ContactInfo { get; set; }
+
+    [Required]
+    [StringLength(10)]
+    [Column("status", TypeName = "character varying(10)")]
+    public string Status { get; set; }
+
+    [Column("is_anonymous")]
+    public bool IsAnonymous { get; set; }
+
+    [Column("is_blocked")]
+    public bool IsBlocked { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString =
+                @"Username=postgres;Password=root;Host=localhost;Port=5432;Database=EroticMassageCompany";
+
+        var myDatabase = new Database(connectionString);
+            
+        var clientsList = myDatabase.Select<Clients>();
+        foreach (var client in clientsList)
+        {
+                Console.WriteLine(client.FullName);
+                Console.Read();
+        }
+
+        // Example of inserting a new client
+        Clients newClient = new Clients
+        {
+            Gender = "Male",
+            FullName = "John Doe",
+            Age = 30,
+            ContactInfo = "john.doe@example.com",
+            Status = "Gold",
+            IsAnonymous = false,
+            IsBlocked = false
+        };
+
+        myDatabase.Insert(newClient);
+
+        // Example of updating a client
+        Clients clientToUpdate = clientsList.FirstOrDefault();
+        if (clientToUpdate != null)
+        {
+            clientToUpdate.FullName = "Updated Name";
+            myDatabase.Update(clientToUpdate);
+        }
+
+        // Example of deleting a client by ID
+        int clientIdToDelete = 1;
+        myDatabase.DeleteById<Clients>(clientIdToDelete);
+    }
+        
+}
---
+++ Homework_7/MyORM.Prog/MyORM/Database.cs
@@ -0,0 +1,123 @@
+﻿using System.Data;
+using System.Reflection;
+using Npgsql;
+
+namespace MyORM;
+
+public class Database
+{
+    public IDbConnection _connection = null;
+    public IDbCommand _cmd = null;
+
+    public Database(string connectionString)
+    {
+        _connection = new NpgsqlConnection(connectionString);
+        _cmd = _connection.CreateCommand();
+    }
+    public void Insert<T>(T model)
+    {
+        
+        using (_connection)
+        {
+            PropertyInfo[] modelFields = model.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            List<string> parametrs = modelFields.Select(x => $"@{x.Name}").ToList();
+
+            string sqlExpression = $"INSERT INTO {typeof(T).Name}s ({string.Join(", ", modelFields.Select(p => p.Name))}) VALUES ({string.Join(", ", parametrs)})";
+
+            foreach (PropertyInfo parametr in modelFields)
+            {
+                _cmd.Parameters.Add(new NpgsqlParameter($"@{parametr.Name}", parametr.GetValue(model)));
+            }
+
+            _cmd.CommandText = sqlExpression;
+
+            _connection.Open();
+            _cmd.ExecuteNonQuery();
+        }
+    }
+
+    public IEnumerable<T> Select<T>()
+    {
+        IList<T> list = new List<T>();
+        Type t = typeof(T);
+
+        using (_connection)
+        {
+            string sqlExpression = $"SELECT * FROM {typeof(T).Name}";
+
+            _cmd.CommandText = sqlExpression;
+    
+            _connection.Open();
+            var reader = _cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                T obj = (T)Activator.CreateInstance(t);
+                t.GetProperties().ToList().ForEach(x => x.SetValue(obj, reader[x.Name]));
+        
+                list.Add(obj);
+            }
+        }
+
+        return list;
+    }
+    public T SelectById<T>(int id)
+    {
+        Type type = typeof(T);
+        T obj = default(T);
+        using (_connection)
+        {
+            string sqlExpression = $"SELECT * FROM {typeof(T).Name}s WHERE id = {id}";
+            _cmd.CommandText = sqlExpression;
+            
+            _connection.Open();
+            var reader = _cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                obj = (T)Activator.CreateInstance(type);
+                type.GetProperties().ToList().ForEach(x => x.SetValue(obj, reader[type.Name]));
+                break;
+            }
+
+            return obj;
+        }
+    }
+
+    public bool DeleteById<T>(int id)
+    {
+        Type type = typeof(T);
+        T obj = default(T);
+        using (_connection)
+        {
+            string sqlExpression = $"DELETE FROM {typeof(T).Name}s WHERE id = {id}";
+            _cmd.CommandText = sqlExpression;
+            
+            _connection.Open();
+            var reader = _cmd.ExecuteNonQuery();
+            return reader > 0;
+        }
+        return true;
+    }
+    public void Update<T>(T t)
+    {
+        using (_connection)
+        {
+            PropertyInfo[] modelFields = t.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            List<string> parametrs = modelFields.Select(x => $"@{x.Name}").ToList();
+
+            string sqlExpression = $"UPDATE {typeof(T).Name}s SET ({string.Join(", ", modelFields
+                .Select((p,i) => p.Name + '=' + parametrs[i]))}) WHERE id = @Id";
+
+            foreach (PropertyInfo parametr in modelFields)
+            {
+                _cmd.Parameters.Add(new NpgsqlParameter($"@{parametr.Name}", parametr.GetValue(t)));
+            }
+
+            _cmd.CommandText = sqlExpression;
+
+            _connection.Open();
+            _cmd.ExecuteNonQuery();
+        }
+    }
+}
