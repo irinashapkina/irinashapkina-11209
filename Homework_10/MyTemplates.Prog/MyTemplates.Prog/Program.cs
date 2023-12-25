3717ee0ce6efc28449142c7963e099c271ab6529
Irina Shapkina <irinkas2005@gmail.com>
2023-12-25 03:06:56 +0300

    creating a templates

---
+++ Homework_10/MyTemplates.Prog/MyTemplates.Prog/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyTemplates
+{
+    public class Students
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+    public class Person
+    {
+        public string Fullname { get; set; }
+        public string Subject { get; set; }
+        public string Group { get; set; }
+        public List<Students> Students { get; set; }
+    }
+
+    public class PersonModel
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var personModel = new PersonModel
+            {
+                FullName = "Иван Иванов",
+                Age = 19,
+                Address = "Улица Пушкина, дом Колотушкина"
+            };
+            var model = new Person()
+            {
+                Fullname = "Иван Иванов",
+                Subject = "ОРИС",
+                Group = "11-209",
+                Students = new List<Students>
+                {
+                    new Students { Name = "Петр", Surname = "Петров" },
+                    new Students { Name = "Анна", Surname = "Сидорова" },
+                }
+            };
+
+            var farewellMessage = MyTemplates.GetFarewellMessage("Иван Иванов");
+            var packageMessagePerson = MyTemplates.GetPackageMessage(personModel);
+            var videoMessage = MyTemplates.GetVideoMessage(personModel);
+            var groupMessage = MyTemplates.GetGroupMessage(model);
+
+            Console.WriteLine(farewellMessage);
+            Console.WriteLine(packageMessagePerson);
+            Console.WriteLine(videoMessage);
+            Console.WriteLine(groupMessage);
+        }
+    }
+}
---
+++ Homework_10/MyTemplates.Prog/MyTemplates/MyTemplates.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MyTemplates;
+
+public class MyTemplates
+{
+    public static string GetFarewellMessage(string name)
+    {
+        return $"Здравствуйте, {name}! Вы уволены".Replace("@{name}", name);
+    }
+
+    public static string GetPackageMessage(object model)
+    {
+        Type modelType = model.GetType();
+
+        PropertyInfo fullNameProp = modelType.GetProperty("FullName");
+        PropertyInfo addressProp = modelType.GetProperty("Address");
+
+        if (fullNameProp != null && addressProp != null)
+        {
+            string fullName = fullNameProp.GetValue(model)?.ToString();
+            string address = addressProp.GetValue(model)?.ToString();
+
+            if (fullName != null && address != null)
+            {
+                return $"Здравствуйте, {fullName} проживающий по адресу {address}, Ваша посылка дошла в пункт выдачи";
+            }
+        }
+
+        return "Неверный тип модели или отсутствуют необходимые свойства";
+    }
+    public static string GetVideoMessage(object model)
+    {
+        var template = "Здравствуйте, @{if(Age >= 18)} @then{Вам стали доступны home video} @else{ваша ссылка на Смешариков: https://www.smeshariki.ru}";
+
+        var regex = new Regex(@"@{if\(([^}]*)\)} @then{([^}]*)} @else{([^}]*)}");
+
+        var result = regex.Replace(template, match =>
+        {
+            var condition = match.Groups[1].Value.Trim();
+            var thenValue = match.Groups[2].Value.Trim();
+            var elseValue = match.Groups[3].Value.Trim();
+
+            var isConditionTrue = EvaluateCondition(condition, model);
+
+            return isConditionTrue ? thenValue : elseValue;
+        });
+
+        return result;
+    }
+
+    private static bool EvaluateCondition(string condition, object model)
+    {
+        try
+        {
+            var conditionRegex = new Regex(@"([^<>=!]+)\s*([<>=!]+)\s*(.+)");
+            var match = conditionRegex.Match(condition);
+
+            if (match.Success)
+            {
+                var propertyName = match.Groups[1].Value.Trim();
+                var operatorString = match.Groups[2].Value.Trim();
+                var valueString = match.Groups[3].Value.Trim();
+
+                var propertyValue = model.GetType().GetProperty(propertyName)?.GetValue(model);
+
+                switch (operatorString)
+                {
+                    case "==":
+                        return object.Equals(propertyValue, Convert.ChangeType(valueString, propertyValue.GetType()));
+                    case "!=":
+                        return !object.Equals(propertyValue, Convert.ChangeType(valueString, propertyValue.GetType()));
+                    case ">":
+                        return Comparer.Default.Compare(propertyValue, Convert.ChangeType(valueString, propertyValue.GetType())) > 0;
+                    case "<":
+                        return Comparer.Default.Compare(propertyValue, Convert.ChangeType(valueString, propertyValue.GetType())) < 0;
+                    case ">=":
+                        return Comparer.Default.Compare(propertyValue, Convert.ChangeType(valueString, propertyValue.GetType())) >= 0;
+                    case "<=":
+                        return Comparer.Default.Compare(propertyValue, Convert.ChangeType(valueString, propertyValue.GetType())) <= 0;
+                    default:
+                        throw new InvalidOperationException($"Invalid operator: {operatorString}");
+                }
+            }
+
+            return false;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    public static string GetGroupMessage(object model)
+    {
+        var template = "Здравствуйте, @{Fullname}! Ваше название предмета @{Subject}. Номер группы @{Group}.";
+
+        var result = template;
+
+        foreach (var property in model.GetType().GetProperties())
+        {
+            var placeholder = "@{" + property.Name + "}";
+            var value = property.GetValue(model)?.ToString() ?? string.Empty;
+
+            result = result.Replace(placeholder, value);
+        }
+
+        var students = model.GetType().GetProperty("Students")?.GetValue(model) as IEnumerable<object>;
+        if (students != null)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Список группы:");
+
+            foreach (var student in students)
+            {
+                var surname = student.GetType().GetProperty("Surname")?.GetValue(student);
+                var name = student.GetType().GetProperty("Name")?.GetValue(student);
+
+                if (surname != null && name != null)
+                {
+                    sb.AppendLine($"-{surname} {name}");
+                }
+            }
+            result = $"{result} {sb}";
+        }
+
+        return result;
+    }
+}
